825b3354bc7c65c7c50f8ae3ac13a75ccd88ee52
moharashid <rashidmohamud9865@gmail.com>
2025-12-30 18:24:59 +0100

    First Commit - My eCommerce Flask Project

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.project_name}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.project_name
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Grant AKS permission to pull from ACR
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type    = string
+  default = "flask-ecommerce"
+}
+
+variable "location" {
+  type    = string
+  default = "polandcentral"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "ecommerceflaskacr123"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "ecommerce-rg"
+}
